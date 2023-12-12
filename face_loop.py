6169d30f8eac4f289242530e9bcb48c79fab24c9
Mariya Miroshnikova <mari.mirosh@bk.ru>
2023-12-12 04:06:43 +0300

    add connectivity to stroke_obj on uv projection

--- face_loop.py
+++ face_loop.py
@@ -4,10 +4,10 @@
 
 import bpy
 import bmesh
-from bmesh.types import BMEdge, BMFace, BMLoop, BMesh, BMLayerItem
+from bmesh.types import BMEdge, BMFace, BMLoop, BMesh, BMLayerItem, BMVert
 from bpy import context
 
-from bpy.types import Mesh, Object, Collection, VertexGroup, Curve
+from bpy.types import Mesh, Object, Collection
 
 from mathutils import Vector
 from typing import List, Set
@@ -22,7 +22,7 @@ def is_quad(face: BMFace) -> bool:
     '''
     return (len(face.loops) == 4)
 
-def collect_face_loop(starting_edge: BMEdge) -> (List[BMFace], int):
+def collect_face_loop(starting_edge: BMEdge) -> (List[BMFace], List[BMLoop], int):
     '''
     Функция обхода face loop (можно переделать в edge ring), начиная с первого ребра для edge ring
     Работает только для квадов, попав на не квад/конец меша - меняет направление, попав снова - останавливается
@@ -31,21 +31,24 @@ def collect_face_loop(starting_edge: BMEdge) -> (List[BMFace], int):
     Если петля зациклена, возвращает -1 (надо соединить последнюю вершину с первой при создании кривой)
     '''
     faces_in_loop = []
+    loops = []
     #обход в одну сторону
     loop = starting_edge.link_loops[0]
 
-    faces_in_loop_one_direction, was_cycled_loop = loop_go(loop, False)
+    faces_in_loop_one_direction, loops_one_direction, was_cycled_loop = loop_go(loop, False)
     faces_in_loop.extend(faces_in_loop_one_direction)
+    loops.extend(loops_one_direction)
     change_direction_face = len(faces_in_loop) - 1
     if (was_cycled_loop):
-        return (faces_in_loop, -1)
+        return (faces_in_loop, loops, -1)
     #обход в другую сторону, если не было цикла
     loop = starting_edge.link_loops[0].link_loop_next.link_loop_next
-    faces_in_loop_two_direction, was_cycled_loop = loop_go(loop, True)
+    faces_in_loop_two_direction, loops_two_direction, was_cycled_loop = loop_go(loop, True)
     faces_in_loop.extend(faces_in_loop_two_direction)
-    return (faces_in_loop, change_direction_face)
+    loops.extend(loops_two_direction)
+    return (faces_in_loop, loops, change_direction_face)
 
-def loop_go(starting_loop: BMLoop, is_second_go: bool) -> (List[BMFace], bool):
+def loop_go(starting_loop: BMLoop, is_second_go: bool) -> (List[BMFace], List[BMLoop], bool):
     '''
     Функция обхода face loop (можно переделать в edge ring), начиная с первого ребра для edge ring
     Работает только для квадов, попав на не квад - останавливается
@@ -53,29 +56,35 @@ def loop_go(starting_loop: BMLoop, is_second_go: bool) -> (List[BMFace], bool):
     is_second_go нужен, чтобы при обходе в обратную сторону не добавлять первую грань еще раз в список
     '''
     faces_in_loop = []
+    loops = []
     loop = starting_loop
     #loop.edge.select = True
     
     # ??????????????????????????? мб проблема с направлениями, мб надо судя именно face подавать чтобы было точнее, а не ребро!
     if (not is_quad(loop.face)):
-        return [], False
+        return [], [], False
     #
     if (not is_second_go):
         faces_in_loop.append(loop.face)
+        loops.append(loop)
     
     radial_loop = loop.link_loop_radial_next
     # проверяем, что mesh оборвался (плоская поверхность)
     if (radial_loop.face == loop.face):
-        return faces_in_loop, False
+        return faces_in_loop, loops, False
     # проверяем, следующая грань это квада? 
     is_next_face_quad = is_quad(radial_loop.face)
     if (not is_next_face_quad):
         # уперлись в не квадратную грань, конец обхода
-        return faces_in_loop, False
+        return faces_in_loop, loops, False
     else:
         faces_in_loop.append(radial_loop.face)
+        if (is_second_go):
+            loops.append(radial_loop)
+        else:
+            loops.append(radial_loop.link_loop_next.link_loop_next)
     next_loop = radial_loop.link_loop_next.link_loop_next
-    next_loop.edge.select = True
+#    next_loop.edge.select = True
     
     # цикл прыжков для сбора всей лупы пока не упремся в не кваду или не вернемся в начальную (замкнутая лупа)
     # или не закончится плоский меш
@@ -90,18 +99,22 @@ def loop_go(starting_loop: BMLoop, is_second_go: bool) -> (List[BMFace], bool):
 
         # проверяем, что mesh оборвался (плоская поверхность)
         if (radial_loop.face == loop.face):
-            return faces_in_loop, False
+            return faces_in_loop, loops, False
         # проверяем, следующая грань это квада? 
         is_next_face_quad = is_quad(radial_loop.face)
         # next_face_orto_loop.face.select = is_quad(next_face_orto_loop.face)
         if (not is_next_face_quad):
-            return faces_in_loop, False
+            return faces_in_loop, loops, False
         else:
             faces_in_loop.append(radial_loop.face)
+            if (is_second_go):
+                loops.append(radial_loop)
+            else:
+                loops.append(radial_loop.link_loop_next.link_loop_next)
 
-        next_loop.edge.select = True
+ #       next_loop.edge.select = True
         loop = next_loop
-    return faces_in_loop, True
+    return faces_in_loop, loops, True
    
 ##################################
 # функции создания нового пустого объекта с пустым мешем без вертекс групп
@@ -280,6 +293,78 @@ def add_vertices_made_in_line(bm: BMesh, vertices: List[Vector], idx_change_dir:
     for i in range(idx_start_edge, idx_start_edge + len(vertices) - 1):
         bm.edges[i].index = i
 
+#nextnextnext.vert == nextnextradial.vert
+#предполагаются что на вход даны две однонаправленные лупы из одинаковых положений в грани и обе принадлежат квадам ПОСЛЕДОВАТЕЛЬНЫМ
+#TODO: функция ЖЕСТКО зависит от внутренностей функции collect_face_loop, изменение порядка обхода там приведет к поломке здесь
+def is_loops_connective_in_uv(uv_layer: BMLayerItem, loop1: BMLoop, loop2: BMLoop, is_after_change_dir: bool) -> bool:
+    '''
+    Функция проверятет, являются ли две данные loop, принадлежащие последовательным граням 3D модели, связными в uv координатах
+    '''
+    if (is_after_change_dir): # после смены направления loop2 = loop1.next.next.radial
+        loop1_next_next_next = loop1.link_loop_next.link_loop_next.link_loop_next
+        return (loop2[uv_layer].uv == loop1_next_next_next[uv_layer].uv)
+    else: # до смены направления loop2 = loop1.radial.next.next
+        loop2_next_next_next = loop2.link_loop_next.link_loop_next.link_loop_next
+        return (loop2_next_next_next[uv_layer].uv == loop1[uv_layer].uv)
+
+# улучшение add_vertices_made_in_line, создает ребра только между точками, чьи грани связны в UV развертке.
+# для этого принимает вместо с uv координатами точек их оригинальные loop
+# TODO: не учитывает возникновение одиночных вершин на острове...
+def add_vertices_made_in_line_with_island_connectivity(bm: BMesh, vertices: List[Vector], loops: List[BMLoop], idx_change_dir: int, uv_layer: BMLayerItem) -> None:
+    '''
+    функция добавляет точки И ребра между ними в bm
+    !!! ожидается cписок ПОСЛЕДОВАТЕЛЬНЫХ вершин от 0 до idx_change_dir включительно, далее от 0-ой до конца!
+    !!! это обусловлено тем, что vertices получены при обходе петли начиная с некоторой грани в одну сторону, а затем от нее же в другую,
+    !!! при этом стартовая грань будет 0-ой в списке.
+    !!! Если idx = -1, то кривая должна быть зациклена, т. е. последняя вершина в списке соединена с 0-ой.
+    TODO: потенциально можно в функции сбора граней сделать сортировку граней. Это упростит понимание и поддержку данной функции
+    но создает необходимость поддерживать лишнюю память для временного несортированного списка граней.
+    '''
+    if (len(vertices) == 0):
+        print("Attempt to make stroke-mesh from empty vertices list.")
+        return
+    idx_start_vert = len(bm.verts)
+    idx_start_edge = len(bm.edges)
+    bm_verts_new = []
+    # добавление вершин
+    for v in vertices:
+        new_vert = bm.verts.new(v)
+        bm_verts_new.append(new_vert)
+
+    # создание зацикленной кривой
+    count_edges = 0
+    if (idx_change_dir == -1):
+        for i in range(0, len(bm_verts_new) - 1):
+            if (is_loops_connective_in_uv(uv_layer, loops[i], loops[i + 1], False)):
+                count_edges += 1
+                bm.edges.new((bm_verts_new[i], bm_verts_new[i + 1]))
+        if (is_loops_connective_in_uv(uv_layer, loops[0], loops[-1], False)):
+            count_edges += 1
+            bm.edges.new((bm_verts_new[0], bm_verts_new[-1]))
+    else: # создание не зацикленной кривой
+        # добавление последовательных ребер в первом направлении
+        for i in range(0, idx_change_dir):
+            if (is_loops_connective_in_uv(uv_layer, loops[i], loops[i + 1], False)):
+                count_edges += 1
+                bm.edges.new((bm_verts_new[i], bm_verts_new[i + 1]))
+        # добавление последовательных ребер в обратном направлении
+        v_start = bm_verts_new[0] # вершина с грани, с которой начинался обход петли
+        loop_start = loops[0]
+        for i in range(idx_change_dir + 1, len(bm_verts_new)):
+            if (is_loops_connective_in_uv(uv_layer, loop_start, loops[i], True)):
+                count_edges += 1
+                bm.edges.new((v_start, bm_verts_new[i]))
+            v_start = bm_verts_new[i]
+            loop_start = loops[i]
+
+    # обновление индексов вершин и ребер [так сказано в документации]
+    bm.verts.ensure_lookup_table()
+    bm.edges.ensure_lookup_table()
+    for i in range(idx_start_vert, idx_start_vert + len(vertices)):
+        bm.verts[i].index = i
+    for i in range(idx_start_edge, idx_start_edge + count_edges):
+        bm.edges[i].index = i
+
 def add_vertices_made_in_line_with_delta_z(bm: BMesh, vertices: List[Vector], idx_change_dir: int, delta_z: float) -> None:
     '''
     Обертка над add_vertices_made_in_line
@@ -289,6 +374,12 @@ def add_vertices_made_in_line_with_delta_z(bm: BMesh, vertices: List[Vector], id
     Предназначена для добавления в объект созданной цепи точек, созданной из заданных точек (как и add_vertices_made_in_line)
     '''
     add_vertices_made_in_line(bm, [Vector([v[0], v[1], delta_z]) for v in vertices], idx_change_dir)
+
+# улучшение функции add_vertices_made_in_line_with_delta_z, строит цепь с учетом того, связаны ли
+# последовательные на 3D модели грани в UV развертке.
+# TODO: не учитывает возникновение одиночных вершин на острове...
+def add_vertices_made_in_line_with_delta_z_and_iscland_connectivity(bm: BMesh, vertices: List[Vector], loops: List[BMLoop], idx_change_dir: int, delta_z: float, uv_layer: BMLayerItem) -> None:
+    add_vertices_made_in_line_with_island_connectivity(bm, [Vector([v[0], v[1], delta_z]) for v in vertices], loops, idx_change_dir, uv_layer)
 
 ################################
 
@@ -337,7 +428,25 @@ def process_faces_from_loop_em(faces: List[BMFace], idx_change_dir: int, delta_z
     print("uv centers count: ", len(uv_centers_of_loop_faces), " points: ", len(faces), " delta z: ", delta_z)
 
     # строим цепь из центров, соединенных ребрами
-    add_vertices_made_in_line_with_delta_z(strokes_bm, uv_centers_of_loop_faces, idx_change_dir, delta_z) 
+    add_vertices_made_in_line_with_delta_z(strokes_bm, uv_centers_of_loop_faces, idx_change_dir, delta_z)
+
+# em - edit mode
+# функция, которую предполагается вызывать для результатов, полученных из функции collect_face_loop
+def process_faces_from_loop_with_island_connectivity_em(faces: List[BMFace], idx_change_dir: int, delta_z: float, mesh_to_project: BMesh, strokes_bm: BMesh, loops: List[BMLoop]):
+    '''
+    Эта функция принимает на вход список граней (объекта mesh_to_project), входящих в петлю,
+    находит их центры в UV координатах,
+    создает из них цепь и добавляет в объект-накопитель-штрихов strokemesh_bm
+    '''
+    # проецируем грани на UV и получаем их центры в UV координатах
+    uv_centers_of_loop_faces = get_uv_vertices_from_faces_em(faces, mesh_to_project)
+    
+    uv_layer = mesh_to_project.loops.layers.uv.verify()
+
+    print("uv centers count: ", len(uv_centers_of_loop_faces), " points: ", len(faces), " delta z: ", delta_z)
+
+    # строим цепь из центров, соединенных ребрами
+    add_vertices_made_in_line_with_delta_z_and_iscland_connectivity(strokes_bm, uv_centers_of_loop_faces, loops, idx_change_dir, delta_z, uv_layer)
 
 def convert_mesh_to_curve_and_make_poly(strokes_obj: Object, mesh_obj: Object):
     '''
@@ -386,17 +495,17 @@ def main():
         
     # предположим, что выбрано ребро на квадратной грани, а то в итоге пустая лупа будет!        
         
-    (faces_in_loop, change_direction_face) = collect_face_loop(starting_edge)
-    process_faces_from_loop_em(faces_in_loop, change_direction_face, 0, bm, strokes_bm)
+    (faces_in_loop, loops, change_direction_face) = collect_face_loop(starting_edge)
+    process_faces_from_loop_with_island_connectivity_em(faces_in_loop, change_direction_face, 0, bm, strokes_bm, loops)
 
-    for face in faces_in_loop:
-        face.select = True
+ #   for face in faces_in_loop:
+ #       face.select = True
 
-    (faces_in_loop, change_direction_face) = collect_face_loop(next_edge)
-    process_faces_from_loop_em(faces_in_loop, change_direction_face, 0.1, bm, strokes_bm)
+    (faces_in_loop, loops, change_direction_face) = collect_face_loop(next_edge)
+    process_faces_from_loop_with_island_connectivity_em(faces_in_loop, change_direction_face, 0.1, bm, strokes_bm, loops)
 
-    for face in faces_in_loop:
-        face.select = True
+#    for face in faces_in_loop:
+#        face.select = True
 
     # обновление объекта на экране
     bmesh.update_edit_mesh(mesh_obj.data)
@@ -409,11 +518,11 @@ def main():
     strokes_bm.free()
 
     #--- EDIT MODE
-    bpy.ops.object.editmode_toggle()
-    bpy.context.view_layer.update()
+  #  bpy.ops.object.editmode_toggle()
+  #  bpy.context.view_layer.update()
     
     # Конвертирование накопителя строк в кривую
-    convert_mesh_to_curve_and_make_poly(strokes_obj, mesh_obj)    
+  #  convert_mesh_to_curve_and_make_poly(strokes_obj, mesh_obj)    
 
 if __name__ == "__main__":
     main()
